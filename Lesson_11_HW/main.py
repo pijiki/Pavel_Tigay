a6bfa02cfff350864188982de8820967ece2a7f3
Pavel-Tigay <lion91191@gmail.com>
2023-04-14 16:51:36 +0500

    Lesson 12 HW

--- Lesson_11_HW/main.py
+++ Lesson_11_HW/main.py
@@ -9,47 +9,47 @@
 # Сделать чтоб он запускался в конструкторе (автоматически)
 # Создать метод который будет сравнивать два класса по их ОЗУ
 
-from functools import total_ordering
-
-
-@total_ordering
-class Computer:
-
-    def __init__(self, user, CPU, RAM, ROM, monitor):
-        self.user = user
-        self.CPU = CPU
-        self.RAM = RAM
-        self.ROM = ROM
-        self.monitor = monitor
-        self.say_my_name()
-
-    def __str__(self):
-        return f'Класс Computer со свойствами user={self.user}, ' \
-               f'CPU = {self.CPU}, RAM = {self.RAM}, ROM = {self.ROM}, monitor = {self.monitor}'
-
-    def say_my_name(self):
-        print(f'Пользователя зовут {self.user}')
-
-    def __eq__(self, other):
-        return self.RAM == other.RAM
-
-    def __lt__(self, other):
-        return self.RAM < other.RAM
-
-
-user1 = Computer(input('Введите имя 2-го пользователя: '), 'i5-11450', 8, 1000, '24 inch')
-user2 = Computer(input('Введите имя 2-го пользователя: '), 'i7-12450', 16, 1500, '32 inch')
-
-print(user1)
-print(user2)
-
-
-print(user1 > user2)
-print(user1 < user2)
-print(user1 >= user2)
-print(user1 <= user2)
-print(user1 == user2)
-print(user1 != user2)
+# from functools import total_ordering
+#
+#
+# @total_ordering
+# class Computer:
+#
+#     def __init__(self, user, CPU, RAM, ROM, monitor):
+#         self.user = user
+#         self.CPU = CPU
+#         self.RAM = RAM
+#         self.ROM = ROM
+#         self.monitor = monitor
+#         self.say_my_name()
+#
+#     def __str__(self):
+#         return f'Класс Computer со свойствами user={self.user}, ' \
+#                f'CPU = {self.CPU}, RAM = {self.RAM}, ROM = {self.ROM}, monitor = {self.monitor}'
+#
+#     def say_my_name(self):
+#         print(f'Пользователя зовут {self.user}')
+#
+#     def __eq__(self, other):
+#         return self.RAM == other.RAM
+#
+#     def __lt__(self, other):
+#         return self.RAM < other.RAM
+#
+#
+# user1 = Computer(input('Введите имя 2-го пользователя: '), 'i5-11450', 8, 1000, '24 inch')
+# user2 = Computer(input('Введите имя 2-го пользователя: '), 'i7-12450', 16, 1500, '32 inch')
+#
+# print(user1)
+# print(user2)
+#
+#
+# print(user1 > user2)
+# print(user1 < user2)
+# print(user1 >= user2)
+# print(user1 <= user2)
+# print(user1 == user2)
+# print(user1 != user2)
 
 # Задание 2.
 # Создать Класс животное с одним параметром (по схеме)
@@ -57,72 +57,72 @@ print(user1 != user2)
 # затем создать по два объекта (экземпляра) от каждого подкласса
 
 
-class Animal:
-    def __init__(self, name):
-        self.name = name
-
-class Birds(Animal):
-    def __init__(self, name, color, arial, size_wings, eats):
-        super(Birds, self).__init__(name)
-        self.color = color
-        self.arial = arial
-        self.wings = size_wings
-        self.eat = eats
-
-    def __str__(self):
-        return f'Класс Birds с name={self.name}, color={self.color}, arial={self.arial}, ' \
-               f'size wings={self.wings}, eats={self.eat}'
-
-class Mamal(Animal):
-    def __init__(self, name, color, arial, size, eats):
-        super(Mamal, self).__init__(name)
-        self.color = color
-        self.country = arial
-        self.size = size
-        self.eat = eats
-
-    def __str__(self):
-        return f'Класс Mamal с name={self.name}, color={self.color}, arial={self.arial}, ' \
-               f'size={self.size}, eats={self.eat}'
-
-class Reptile(Animal):
-    def __init__(self, name, arial, size_scales, size, eats):
-        super(Reptile, self).__init__(name)
-        self.arial = arial
-        self.scales = size_scales
-        self.size = size
-        self.eat = eats
-
-    def __str__(self):
-        return f'Класс Reptile с name={self.name}, arial={self.arial}, size scales={self.scales}, ' \
-               f'size ={self.size}, eats={self.eat}'
-
-class Fish(Animal):
-    def __init__(self, name, arial, color, eats):
-        super(Fish, self).__init__(name)
-        self.arial = arial
-        self.color = color
-        self.eat = eats
-
-
-    def __str__(self):
-        return f'Класс Fish с name={self.name}, arial={self.arial}, color={self.color}, eats={self.eat}'
-
-
-bird_eagle = Birds('Eagle', 'brown', 'USA', 40, "meat")
-bird_chiken = Birds('Chiken', 'white', 'everywhere', 20, 'corn')
-
-
-mamal_rat = Mamal('rat', 'grey', 'everywhere', 15, 'omnivorous')
-mamal_monkey = Mamal('monkey', 'orange', 'jungle', 50, 'fruits')
-
-
-reptile_snake = Reptile('snake', 'jungle', '6 mm', '1 m', 'meat')
-reptile_chameleon = Reptile('chameleon', 'jungle', '2 mm', '200 mm', 'insects')
-
-
-fish_clown = Fish('fish_clown', 'ocean', 'white-orange', 'plankton')
-fish_shark = Fish('white_shark', 'ocean', 'white-blue', 'meat')
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#
+# class Birds(Animal):
+#     def __init__(self, name, color, arial, size_wings, eats):
+#         super(Birds, self).__init__(name)
+#         self.color = color
+#         self.arial = arial
+#         self.wings = size_wings
+#         self.eat = eats
+#
+#     def __str__(self):
+#         return f'Класс Birds с name={self.name}, color={self.color}, arial={self.arial}, ' \
+#                f'size wings={self.wings}, eats={self.eat}'
+#
+# class Mamal(Animal):
+#     def __init__(self, name, color, arial, size, eats):
+#         super(Mamal, self).__init__(name)
+#         self.color = color
+#         self.country = arial
+#         self.size = size
+#         self.eat = eats
+#
+#     def __str__(self):
+#         return f'Класс Mamal с name={self.name}, color={self.color}, arial={self.arial}, ' \
+#                f'size={self.size}, eats={self.eat}'
+#
+# class Reptile(Animal):
+#     def __init__(self, name, arial, size_scales, size, eats):
+#         super(Reptile, self).__init__(name)
+#         self.arial = arial
+#         self.scales = size_scales
+#         self.size = size
+#         self.eat = eats
+#
+#     def __str__(self):
+#         return f'Класс Reptile с name={self.name}, arial={self.arial}, size scales={self.scales}, ' \
+#                f'size ={self.size}, eats={self.eat}'
+#
+# class Fish(Animal):
+#     def __init__(self, name, arial, color, eats):
+#         super(Fish, self).__init__(name)
+#         self.arial = arial
+#         self.color = color
+#         self.eat = eats
+#
+#
+#     def __str__(self):
+#         return f'Класс Fish с name={self.name}, arial={self.arial}, color={self.color}, eats={self.eat}'
+#
+#
+# bird_eagle = Birds('Eagle', 'brown', 'USA', 40, "meat")
+# bird_chiken = Birds('Chiken', 'white', 'everywhere', 20, 'corn')
+#
+#
+# mamal_rat = Mamal('rat', 'grey', 'everywhere', 15, 'omnivorous')
+# mamal_monkey = Mamal('monkey', 'orange', 'jungle', 50, 'fruits')
+#
+#
+# reptile_snake = Reptile('snake', 'jungle', '6 mm', '1 m', 'meat')
+# reptile_chameleon = Reptile('chameleon', 'jungle', '2 mm', '200 mm', 'insects')
+#
+#
+# fish_clown = Fish('fish_clown', 'ocean', 'white-orange', 'plankton')
+# fish_shark = Fish('white_shark', 'ocean', 'white-blue', 'meat')
 
 
 # Задание 3.
---
+++ Lesson_12_HW/weather.py
@@ -0,0 +1,67 @@
+# Pavel
+
+import requests
+from pprint import pprint
+from countryinfo import CountryInfo
+
+while True:
+    country = CountryInfo(input('Введите город: ')).info()
+
+    print(f'''
+Name : \033[31m{country['name']}\033[0m
+NativeName: \033[31m{country['nativeName']}\033[0m
+Capital : \033[32m{country['capital']}\033[0m
+Capital_latlng: \033[32m{country['capital_latlng']}\033[0m
+Region : \033[33m{country['region']}\033[0m
+Provinces: \033[33m{country['provinces']}\033[0m
+Population : \033[34m{country['population']}\033[0m
+Borders: \033[32m{country['borders']}\033[0m
+Language : \033[35m{country['languages']}\033[0m
+TimeZones : {country['timezones']}
+Area : \033[36m{country['area']} km²\033[0m
+Translations: \033[35m{country['translations']}\033[0m
+Wiki : \033[38m{country['wiki']}\033[0m
+''')
+
+
+
+
+
+
+
+url = "https://airbnb13.p.rapidapi.com/search-location"
+
+querystring = {
+	"location": "Paris",
+	"checkin": "2023-09-16",
+	"checkout": "2023-09-17",
+	"adults": "1",
+	"children": "0",
+	"infants": "0",
+	"pets": "0",
+	"page": "1",
+	"currency": "USD"}
+
+headers = {
+	"X-RapidAPI-Key": "bac79eaabfmsh0b5842b3efa7f3ep174c2cjsne3c3dc6d8f6f",
+	"X-RapidAPI-Host": "airbnb13.p.rapidapi.com"
+}
+
+city = input('Введите город для просмотра: ')
+querystring['q'] = city
+response = requests.get(url, headers=headers, params=querystring).json()
+for i in response['results']:
+	address = i['address']
+	type1 = i['type']
+	price = i['price']['total']
+	beds = i['beds']
+	name = i['name']
+	rating = i['rating']
+	amenities = i['previewAmenities']
+	print(f'''
+В городе {city}, расположенный по адресу {address} 
+и названием {name} с рейтингом {rating} и дополнительными удобствами с {amenities}
+есть комнаты с типом {type1} c количеством кроватей {beds}.
+Общая стоимость будет {price}
+	''')
+
